31035daec6d05c85da60a66248b909209e64d264
Angly Angly <andyvalycris@gmail.com>
2024-02-17 17:46:50 +0100

    hola

--- SCAF/packages/nextjs/app/page.tsx
+++ SCAF/packages/nextjs/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import { useState } from "react";
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client";
-import { useState, useEffect } from "react";
-import { useWaitForTransaction } from 'wagmi'
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { Address, AddressInput, Balance } from "~~/components/scaffold-eth";
@@ -15,54 +14,25 @@ import {
 } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
-  const [address_user, set_address_user] = useState("");
-  const { data: greeting, waitForTransaction } = useScaffoldContractRead({
-    contractName: "TokenReleaseContract",
-    functionName: "payForTokens",
-    args: [address_user],
+  const [newGreeting, setNewGreeting] = useState("");
+  const { writeAsync: setGreeting } = useScaffoldContractWrite({
+    contractName: "Bank.sol",
+    functionName: "setGreeting",
+    args: [newGreeting],
   });
-  
-  const handleGetTokenClick = async () => {
-    if (address_user == ""){
-      alert("Wallet address invalid");
-    }
-    else{
-      try {
-        await waitForTransaction();
-        alert("Token created successfully");
-        const holaDiv = document.getElementById('game') as HTMLElement;
-        if (holaDiv) {
-          holaDiv.style.display = 'block';
-        }
-        const wallet_id = document.getElementById('wallet') as HTMLElement;
-        if (wallet_id) {
-          wallet_id.style.display = 'none';
-        }
-      } catch (error) {
-        alert("Wallet address invalid");
-      }
-    }
-  };
-
-  return (
-    <>
-      <title>SCAF</title>
-      <div id="wallet" className="flex items-center flex-col flex-grow pt-10">
+  return <>
+      <div className="flex items-center flex-col flex-grow pt-10">
         <div className="p-5">
           <input
-            value={address_user}
-            onChange={(e) => set_address_user(e.target.value)}
-            placeholder="Input your wallet address"
+            value={newGreeting}
+            placeholder="Wallet address"
             className="input"
+            onChange={(e) => setNewGreeting(e.target.value)}
           />
-          <button className="btn btn-primary" onClick={handleGetTokenClick}>Get Token</button>
+          <button className="btn btn-primary" onClick={setGreeting}>Get Token</button>
         </div>
       </div>
-      <div id="game" className="flex items-center flex-col flex-grow pt-10" style={{ display: 'none'}}>
-<h1>hola</h1>
-      </div>
-    </>
-  );
+  </>;
 };
 
 export default Home;
