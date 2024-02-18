dd16ab4062912c7f973a2bbf93d9d114abd986d2
crea <crea@student.42roma.it>
2024-02-18 07:56:47 +0100

    updating event listener

--- SCAF/packages/nextjs/app/game/page.tsx
+++ SCAF/packages/nextjs/app/game/page.tsx
@@ -1,5 +1,20 @@
-import type { NextPage } from 'next';
-import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+"use client";
+
+import { useState } from "react";
+import Link from 'next/link';
+import { ethers } from "ethers";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+import type { NextPage } from "next";
+import { useAccount } from "wagmi";
+import { Address, AddressInput, Balance } from "~~/components/scaffold-eth";
+import {
+  useAccountBalance,
+  useDeployedContractInfo,
+  useScaffoldContractRead,
+  useScaffoldContractWrite,
+  useScaffoldEventSubscriber,
+} from "~~/hooks/scaffold-eth";
 
 const Game: NextPage = () => {
 
@@ -7,6 +22,23 @@ const Game: NextPage = () => {
     contractName: "YourContract",
     functionName: "burnPlayToken",
   });
+
+  // Definisci la funzione che vuoi eseguire quando l'evento viene ricevuto
+  const handleBeginPlay = () => {
+    console.log('Evento BeginPlay ricevuto:');
+    BeginPlay(); // Chiama qui la tua funzione BeginPlay
+  };
+
+  useScaffoldEventSubscriber ({
+    contractName: "YourContract",
+    eventName: "BeginPlay",
+    listener: handleBeginPlay,
+  })
+
+  function BeginPlay() {
+    // Logica per gestire l'inizio del gioco
+    console.log("Inizio del gioco");
+  }
 
   return (
     <div>
